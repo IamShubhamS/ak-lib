4e8964506822ec6c37b62cbd47c3eef0f79770b4
IamShubhamS <shubham@accuknox.com>
2026-04-17 11:14:44 +0530

    Update runaccuknoxsecretscan.groovy

--- vars/runaccuknoxsecretscan.groovy
+++ vars/runaccuknoxsecretscan.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) {
     String image = config.get('image', 'gcshubham/accuknox-secret-jenkins:0.3')
+    boolean softFail = config.get('softFail', true)
 
     if (!env.ACCUKNOX_ENDPOINT?.trim()) {
         error('ACCUKNOX_ENDPOINT is required')
@@ -11,8 +12,7 @@ def call(Map config = [:]) {
         error('ACCUKNOX_TOKEN is required')
     }
 
-    sh """
-      set -e
+    String cmd = """
       docker pull ${image}
       docker run --rm \\
         -e ACCUKNOX_ENDPOINT="\$ACCUKNOX_ENDPOINT" \\
@@ -20,5 +20,12 @@ def call(Map config = [:]) {
         -e ACCUKNOX_TOKEN="\$ACCUKNOX_TOKEN" \\
         -v "\$WORKSPACE:/workspace" \\
         ${image}
-    """
+    """.stripIndent()
+
+    if (softFail) {
+        int rc = sh(script: cmd, returnStatus: true)
+        echo "akSecretScan completed with exit code: ${rc}. Continuing because softFail=true."
+    } else {
+        sh cmd
+    }
 }
